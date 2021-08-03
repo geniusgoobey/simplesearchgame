fa5cc8f6c22f6bd6a9144bd0ba109b751ebced16
Sridhar <Sridhar@ASUS>
2021-08-02 23:10:19 -0400

    added code to pick up a random word from the wordlist.txt text file and generate the pattern for guessing (uses 20% of length of the word)

--- simplewordgame/simplewordgame/Program.cs
+++ simplewordgame/simplewordgame/Program.cs
@@ -1,12 +1,55 @@
 ﻿using System;
+using System.IO;
+
+
 
 namespace simplewordgame
 {
     class Program
-    {
+    { 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            char[] chr_gameWord = getNewWord();
+
+            char[] chr_puzzle = initialize_newGame(chr_gameWord);
+                     
+   
+
+           
+        }
+
+        private static char[] initialize_newGame(char[] chr_gameWord)
+        {
+            Random rnd = new Random(DateTime.Now.Millisecond);
+            char[] puzzle = new char[chr_gameWord.Length];
+            int size = (int) Math.Round(chr_gameWord.Length * 0.2);
+           // int[] pos = new int[size];
+            int pos = 0;
+            for (int i = 0; i < chr_gameWord.Length; i++)
+            {
+                puzzle[i] = '_';
+
+            }
+            
+            for (int i = 0; i< size; i++)
+            {
+                pos = rnd.Next(0, chr_gameWord.Length);
+                puzzle[pos] = chr_gameWord[pos];
+
+            }
+
+            return puzzle;
+
+        }
+
+        private static char[]  getNewWord()
+        {
+
+            string[] wordList = File.ReadAllLines(@"C:\Users\Sridhar\source\repos\simplesearchgame\simplewordgame\simplewordgame\wordlist.txt");
+            Random rnd = new Random(DateTime.Now.Millisecond);
+            int wordIndex = rnd.Next(1, wordList.Length);
+            return wordList[wordIndex].ToUpper().ToCharArray();
+
         }
     }
 }
